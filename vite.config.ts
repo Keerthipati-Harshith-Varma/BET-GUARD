42f1aaa43b6d17c148dd222c1d92c104ea6f6e2d
KEERTHIPATI HARSHITH VARMA <2410030218@klh.edu.in>
2025-11-06 23:53:39 +0530

    Update Vite config to adjust chunk size limit
    Increase chunk size warning limit for builds.

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,15 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
+
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
   },
   preview: {
-    allowedHosts: ["bet-guard-1.onrender.com"], // ✅ Allow Render domain
-    port: 8080, // optional but recommended to keep consistent
+    allowedHosts: ["bet-guard-1.onrender.com"], 
+    port: 8080, 
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
@@ -19,4 +19,7 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    chunkSizeWarningLimit: 2000
+  }
 }));
